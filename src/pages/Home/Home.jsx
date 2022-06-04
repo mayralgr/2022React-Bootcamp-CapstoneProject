bc369eabd6eb9c2a958189924eb7bab9cc0f2115
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-04 18:57:35 -0500

    Move mocks to src and add Slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
+import Slider from '../../components/Slider/Slider';
+
 const Home = () => {
-    return <> <h2>Home Page</h2></>
+  return (
+    <>
+      <h2>Home Page</h2>
+      <Slider />
+    </>
+  );
 };
 
 export default Home;
