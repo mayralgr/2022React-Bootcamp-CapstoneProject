9fa8f33546d44a0bbde4eedb3d737b38f0f8a48f
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-05 11:24:32 -0500

    Add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,85 +4,97 @@ import searchIcon from '../../resources/images/search_icon.svg';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Logo = styled.img`
-  width: 5%;
-  float: left;
-  margin: 1%;
-  margin-rigth: 0;
-  height: 6vh;
-`;
-
 const Wrapper = styled.div`
-  width: 100%;
   background-color: black;
   height: 8vh;
   color: white;
+  width: 100%;
 `;
-const Name = styled.span`
-  // width: 10%;
-  padding: 2%;
-  padding-left: 0;
-  height: 6vh;
+
+const Logo = styled.img`
+  // width: 5%;
   float: left;
-  color: white;
-`;
-const HeaderTab = styled.li`
-  padding: 2%;
+  // margin: 1%;
+  // margin-rigth: 0;
   height: 6vh;
-  color: white;
-  text-align: center;
+`;
+
+const RigthWrapper = styled.div`
+  float: right;
+  padding: 0;
+  margin: 0;
 `;
 
 const Cart = styled.img`
-  width: 5%;
-  float: right;
   text-align: right;
-  margin: 1%;
+  padding: 1%;
   height: 6vh;
 `;
 
 const Search = styled.img`
-  width: 5%;
-  float: right;
-  margin: 1%;
+  margin: 0;
+  padding: 0;
   height: 6vh;
 `;
 
 const NavLink = styled(Link)`
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 12px;
   text-decoration: none;
-  color: white;
+  font-size: 15px;
+  &:hover:first-child:hover {
+    color: white;
+  }
   &:hover,
   &:focus {
     color: #888888;
   }
 `;
 
-const TabsWrapper = styled.ul`
+const NavLinkIcons = styled(Link)`
+  float: left;
   display: flex;
-  flex-wrap: wrap;
-  padding-left: 0;
-  margin-bottom: 0;
-  list-style: none;
-}
+  color: #f2f2f2;
+  text-align: center;
+  padding-top: 5%;
+  text-decoration: none;
+  font-size: 15px;
+  padding-top: 8px;
+`;
+
+const SearchInput = styled.input`
+  float: left;
+  display: flex;
+  color: #f2f2f2;
+  text-align: center;
+  padding-top: 5%;
+  text-decoration: none;
+  font-size: 11px;
+  height: 4vh;
+  margin-top: 6%;
+  border-radius: 20px;
 `;
 
 const Header = () => (
   <Wrapper>
-    <Logo src={logo} alt="logo" />
-    <Name>GetIT</Name>
-
-    <TabsWrapper>
-      <HeaderTab>
-        <NavLink to="/">Home</NavLink>
-      </HeaderTab>
-      <HeaderTab>
-        <NavLink to="/products">Products</NavLink>
-      </HeaderTab>
-      
-    <Search src={searchIcon} alt="searchIcon" />
-    <Cart src={shoppingCart} alt="shoppingCart" />
-    </TabsWrapper>
-    
+    <NavLink to="/">
+      <Logo src={logo} alt="logo" />
+      GetIT
+    </NavLink>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/products">Products</NavLink>
+    <RigthWrapper>
+      <SearchInput type="text" />
+      <NavLinkIcons to="/">
+        <Search src={searchIcon} alt="searchIcon" />
+      </NavLinkIcons>
+      <NavLinkIcons to="/">
+        <Cart src={shoppingCart} alt="shoppingCart" />
+      </NavLinkIcons>
+    </RigthWrapper>
   </Wrapper>
 );
 
