8ac05a67394aa2dd5b97ccc7b6ada4478959f903
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-06 10:11:40 -0500

    Clean up and App router component creation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Checkout from './pages/Checkout/Checkout';
-import ProductList from './pages/ProductList/ProductList';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import AppRouter from './components/AppRouter/AppRouter';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/products" element={<ProductList />} />
-      </Routes>
+      <AppRouter/>
       <Footer />
     </div>
   );
