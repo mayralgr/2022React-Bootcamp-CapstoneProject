95b3a42550b892f4f7a1793c44b6a446930874f1
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-08 23:27:44 -0500

    Add conditional in styled component

--- src/components/Sidebar/SidebarCategory/SidebarCategory.jsx
+++ src/components/Sidebar/SidebarCategory/SidebarCategory.jsx
@@ -3,11 +3,7 @@ import SidebarLink from '../../../styles/SidebarLink.styled';
 const SidebarCategory = ({ handleCategoryClick, category, active }) => {
     return (
         <SidebarLink
-            style={
-                active
-                    ? { backgroundColor: '#bb7c54' }
-                    : { backgroundColor: '#fc9c9c' }
-            }
+            active = {active}
             id={category.id}
             key={category.id + 'inner'}
             onClick={handleCategoryClick}>
--- src/styles/SidebarLink.styled.js
+++ src/styles/SidebarLink.styled.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 const SidebarLink = styled.div`
+    background-color: ${({ active }) =>
+        active
+            ? '#bb7c54'
+            : '#fc9c9c'}; //Conditional styling the background color
     display: block;
     color: black;
     padding: 16px;
