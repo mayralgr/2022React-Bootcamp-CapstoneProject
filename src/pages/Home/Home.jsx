833a6d6c0981e344aa4d40470a864c792d7bdc53
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-13 20:42:01 -0500

    Add custom hook for categories and refactor home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,19 @@
 import ProductCategories from '../../components/ProductCategories/ProductCategories';
 import Slider from '../../components/Slider/Slider';
 import FeaturedProductGrid from '../../components/ProductsGrids/FeaturedProductsGrid';
-import { productCategories } from '../../mocks/en-us/product-categories';
 import { featuredProducts } from '../../mocks/en-us/featured-products';
 import Button from '../../styles/ButtonSeeMore.styled';
 import { useFeaturedBanners } from '../../utils/hooks/useFeaturedBanners';
+import { useCategories } from '../../utils/hooks/useCategories';
 
 const Home = () => {
-    const { data, isLoading } = useFeaturedBanners();
-
+    const { data: featuredBannersData, isLoading: featuredBannersLoading } = useFeaturedBanners();
+    const { data: categoriesData, isLoading: categoriesLoading } = useCategories();
+    
     return (
         <div style={{ paddingBottom: '2rem' }}>
-            <ProductCategories categories={productCategories} />
-            <Slider isLoading={isLoading} featuredBanners={data} />
+            <ProductCategories categories={categoriesData} isLoading={categoriesLoading} />
+            <Slider isLoading={featuredBannersLoading} featuredBanners={featuredBannersData} />
             <FeaturedProductGrid products={featuredProducts} />
             <Button to="/products">View all products</Button>
         </div>
