d28264958e4f3bcf9fb0031767c767126fb8c628
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-16 19:50:48 -0500

    Modify home page

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import Checkout from '../../pages/Checkout/Checkout';
 import ProductList from '../../pages/ProductList/ProductList';
+import ProductDetail from '../../pages/ProductDetail/ProductDetail';
 
 const AppRouter = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/checkout" element={<Checkout />} />
     <Route path="/products" element={<ProductList />} />
-    
+    <Route path="/productDetail" element={<ProductDetail />} />
     {/* <Route path="/products?category=categorySlug" element={<ProductList />} /> */}
     <Route path="/*" element={<Home />} />
     <Route path="*/" element={<Home />} />
