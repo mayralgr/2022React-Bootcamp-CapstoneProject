4c9edc399c33294eced93ed24659cee0f51d579d
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-27 22:58:28 -0500

    Add product detail page

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,9 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useProductDetail } from '../../utils/hooks/useProductDetail';
-// import { useEffect } from 'react';
-
 import Spinner from 'react-bootstrap/Spinner';
-import Product from '../../components/ProductsGrids/Product/Product';
+import ProductDetailInfo from '../../components/ProductDetail/ProductDetailInfo';
 
 const ProductDetail = () => {
     const { productId } = useParams();
@@ -12,17 +10,15 @@ const ProductDetail = () => {
         
     return (
         <>
-            <h2>Product Detail Page</h2>
             {productDataLoading ? (
                 <Spinner animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </Spinner>
             ) : (
-                <div>
-                    <h1>Product here</h1>
+                <>
                 {productData?.results.map((product) => (
-                    <Product key={product.id} product={product} />
-                ))}</div>
+                    <ProductDetailInfo key={product.id} product={product} />
+                ))}</>
             )}
         </>
     );
